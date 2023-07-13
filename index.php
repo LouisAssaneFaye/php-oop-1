f24aa74ba226d0ff343f2791c6c4a799a76532e2
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-07-13 20:47:48 +0200

    Esercizio completato.

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+class Movie {
+    public $title;
+    public $originaLenguage;
+    public $minuts;
+    public $genere;
+
+    function__construct(String $_title, String $_originaLenguage, Int $_minuts, String $_genere){
+        $this->title = $_title;
+        $this->originaLenguage = $_originaLenguage;
+        $this->minuts = $_minuts;
+        $this->genere = $_genere;
+
+    }
+
+    function getTitle(){
+        return $this->title;
+    }
+    function getOriginaLenguage(){
+        return $this->originaLenguage;
+    }
+    function getMinuts(){
+        return $this->minuts;
+    }
+    function getGenere(){
+        return $this->genere;
+    }
+}
+
+$troy = new Movie ('troy', 'english', '90', 'fantasy');
+echo $troy-> getTitle();
+echo $troy-> getOriginaLenguage();
+echo $troy-> getMinuts();
+echo $troy-> getGenere();
+
+$titanic = new Movie ('titanic', 'english', '120', 'drama');
+echo $troy-> getTitle();
+echo $troy-> getOriginaLenguage();
+echo $troy-> getMinuts();
+echo $troy-> getGenere();
